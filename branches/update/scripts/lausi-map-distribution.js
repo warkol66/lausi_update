2aaa061caeec2c871f4f81adcf1ee1aa0bfbfd5e
axel <axel@866e3e16-41d3-4f0a-9ddd-98571eee7c8a>
2011-04-07 18:08:46 +0000

    Cambio a POO dos de los tres js de mapas.
    Todavía quedan cosas por pulir.

--- branches/update/scripts/lausi-map-distribution.js
+++ branches/update/scripts/lausi-map-distribution.js
@@ -1,73 +1,114 @@
-var map;
-var markers = [];
-var icons = {
-	available: 'images/marker_green.png', 
-	assigned: 'images/marker_blue.png',
-	partiallyAssigned: 'images/partiallyAssigned.png',
-	client: 'images/pin_green.png'
+DistributionMap = function() {
+	//Referencia al mapa
+	this.map;
+	
+	//Referencias a los markers
+	this.markers = [];
+	
+	//Mapeo de tipos de markers con sus respectivos iconos.
+	this.icons = {
+		available: 'images/marker_green.png', 
+		assigned: 'images/marker_blue.png',
+		partiallyAssigned: 'images/partiallyAssigned.png',
+		client: 'images/pin_green.png'
+	};
+	
+	//Colores para los trazos de los anillos
+	this.strokeColors = ['#00ff00', '#0000ff', '#fffc00', '#ff0000'];
+	
+	/**
+	 * Inicia y muestra el mapa.
+	 * 
+	 * @param string canvasId, id del div donde renderizar el mapa
+	 */
+	this.initializeMap = function(canvasId) {
+	    var latlng = new google.maps.LatLng('-34.609', '-58.445');
+	    var myOptions = {
+	      zoom: 12,
+	      center: latlng,
+	      mapTypeId: google.maps.MapTypeId.ROADMAP
+	    }
+	    this.map = new google.maps.Map(document.getElementById(canvasId), myOptions);
+	    
+	    this.map.enableKeyDragZoom({
+			boxStyle: {
+				border: "thick blue",
+				backgroundColor: "blue",
+				opacity: 0.3
+			},
+			paneStyle: {
+				backgroundColor: "grey",
+				opacity: 0.1
+			}
+		});
+	}
+	
+	this.displayMarker = function(id, position, type, onClickCallback) {
+		var marker;
+		marker = new google.maps.Marker({
+	       	map: this.map, 
+	       	position: position,
+	       	icon: this.icons[type]
+	    });
+	        
+	    google.maps.event.addListener(marker, 'click', function() {
+	    	onClickCallback(marker);
+	  	});
+	  	
+	  	this.markers[id] = marker;
+	}
+	
+	/**
+	 * Cambia el estado del marker como assignado.
+	 * 
+	 * @param string id, identificador del marker
+	 */
+	this.markAssigned = function(markerId) {
+		var marker = this.markers[markerId];
+		if (marker != undefined)
+			marker.setIcon(icons['assigned']);
+	}
+	
+	/**
+	 * Cambia el estado del marker como assignado.
+	 * 
+	 * @param string id, identificador del marker
+	 */
+	this.markAvailable = function(markerId) {
+		var marker = this.markers[markerId];
+		if (marker != undefined)
+			marker.setIcon(icons['available']);
+	}
+	
+	/**
+	 * Cambia el estado del marker como assignado.
+	 * 
+	 * @param string id, identificador del marker
+	 */
+	this.markPartiallyAssigned = function(markerId) {
+		var marker = this.markers[markerId];
+		if (marker != undefined)
+			marker.setIcon(icons['partiallyAssigned']);
+	}
+	
+	/**
+	 * Dibuja un circulo.
+	 * 
+	 * @param LatLng center, centro del circulo en coordenadas.
+	 * @param int radius, radio del circulo en metros.
+	 * @param int colorIndex, indice del color a utilizar según this.strokeColors, opcional.
+	 */
+	this.drawRing = function(center, radius, colorIndex) {
+		if (colorIndex === undefined)
+			colorIndex = 0;
+		new google.maps.Circle({
+			center: center,
+			map: this.map,
+			radius: radius,
+			strokeColor: this.strokeColors[colorIndex],
+			strokeOpacity: 0.6,
+			fillColor: '#0000ff',
+			fillOpacity: 0.0
+		});
+	}
 };
-
-function initializeMap() {
-    var latlng = new google.maps.LatLng('-34.609', '-58.445');
-    var myOptions = {
-      zoom: 12,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    
-    map.enableKeyDragZoom({
-		boxStyle: {
-			border: "thick blue",
-			backgroundColor: "blue",
-			opacity: 0.3
-		},
-		paneStyle: {
-			backgroundColor: "grey",
-			opacity: 0.1
-		}
-	});
-}
-	
-function displayMarker(id, position, type, onClickCallback) {
-	var marker;
-	marker = new google.maps.Marker({
-       	map: map, 
-       	position: position,
-       	icon: icons[type]
-    });
-        
-    google.maps.event.addListener(marker, 'click', function() {
-    	onClickCallback(marker);
-  	});
-  	
-  	markers[id] = marker;
-}
-
-function markAssigned(markerId) {
-	markers[markerId].setIcon(icons['assigned']);
-}
-
-function markAvailable(markerId) {
-	markers[markerId].setIcon(icons['available']);
-}
-
-function markPartiallyAssigned(markerId) {
-	markers[markerId].setIcon(icons['partiallyAssigned']);
-}
-
-function drawRing(center, radius, colorIndex) {
-	var strokeColors = ['#00ff00', '#0000ff', '#fffc00', '#ff0000'];
-	
-	if (colorIndex === undefined)
-		colorIndex = 0;
-	new google.maps.Circle({
-		center: center,
-		map: map,
-		radius: radius,
-		strokeColor: strokeColors[colorIndex],
-		strokeOpacity: 0.6,
-		fillColor: '#0000ff',
-		fillOpacity: 0.0
-	});
-}
--- branches/update/scripts/lausi-map-route-sheets.js
+++ branches/update/scripts/lausi-map-route-sheets.js
@@ -1,186 +1,207 @@
-var map;
-var polyLine;
-var directionsDisplays = [];
-var directionsService;
-var firstPath = {};
-
-//Lo usamos como indice invertido para buscar por marcador.
-var pathByPosition = {};
-
-function initializeMap() {
-    var latlng = new google.maps.LatLng('-34.609', '-58.445');
-    var myOptions = {
-      zoom: 12,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	map.enableKeyDragZoom({
-		boxStyle: {
-			border: "thick blue",
-			backgroundColor: "blue",
-			opacity: 0.3
-		},
-		paneStyle: {
-			backgroundColor: "grey",
-			opacity: 0.1
+RouteSheetsMap = function() {
+	this.map;
+	this.polyLine;
+	this.directionsDisplays = [];
+	this.directionsService;
+	this.firstPath = {};
+	
+	//Lo usamos como indice invertido para buscar por marcador.
+	this.pathByPosition = {};
+	
+	/**
+	 * Inicia y muestra el mapa.
+	 * 
+	 * @param string canvasId, id del div donde renderizar el mapa
+	 */
+	this.initializeMap = function(canvasId) {
+	    var latlng = new google.maps.LatLng('-34.609', '-58.445');
+	    var myOptions = {
+	      zoom: 12,
+	      center: latlng,
+	      mapTypeId: google.maps.MapTypeId.ROADMAP
+	    }
+	    this.map = new google.maps.Map(document.getElementById(canvasId), myOptions);
+		this.map.enableKeyDragZoom({
+			boxStyle: {
+				border: "thick blue",
+				backgroundColor: "blue",
+				opacity: 0.3
+			},
+			paneStyle: {
+				backgroundColor: "grey",
+				opacity: 0.1
+			}
+		});
+	    
+	    var polyOptions = {
+		    strokeColor: '#0000ff',
+		    strokeOpacity: 1.0,
+		    strokeWeight: 3
+	  	}
+	  	
+	    this.polyLine = new google.maps.Polyline(polyOptions);
+	  	this.polyLine.setMap(this.map);
+	  	
+	  	this.directionsService = new google.maps.DirectionsService();
+	}
+		
+	/**
+	 * Muestra un marker en el mapa, con las opciones especificadas.
+	 * 
+	 * @param string id, un identificador para acceder posteriormente al marker
+	 * @param LatLng position, ubicación en coordenadas.
+	 * @param string type, tipo de marker (afecta al icono a mostrar)
+	 * @param function onClickCallback, una función para ejecutar al clickear el marker.
+	 */	
+	this.displayMarker = function(position) {
+		var marker;
+		marker = new google.maps.Marker({
+	       	map: this.map, 
+	       	position: position,
+	    });
+	    
+	    var routeSheetsMap = this;
+	        
+	    google.maps.event.addListener(marker, 'click', function() {
+	    	routeSheetsMap.markerOnClick(this)
+	    });
+	    
+	  	google.maps.event.addListener(marker, 'mouseover', function() {
+	  		routeSheetsMap.markerMouseOver(this)
+	  	});
+	  	
+	  	google.maps.event.addListener(marker, 'mouseout', function() {
+	  		routeSheetsMap.markerMouseOut(this)
+	  	});
+	  	
+	  	return marker;
+	}
+	
+	this.markerMouseOver = function(marker) {
+		marker.setIcon('images/marker_green.png');
+		$(this.pathByPosition[marker.position.toString()].lid).setStyle({'background': '#00ff00', 'textDecoration': 'underline'});
+	}
+	
+	this.markerMouseOut = function(marker) {
+		marker.setIcon('');
+		$(this.pathByPosition[marker.position.toString()].lid).setStyle({'background': 'transparent', 'textDecoration': 'none'});
+	}
+	
+	this.markerOnClick = function(marker) {
+		var path = this.polyLine.getPath();
+		var i = 0;
+		var found = false;
+		var inFirstPath = false;
+		
+		while (i < path.getLength() && !found) {
+			if (path.getAt(i).equals(marker.getPosition()))
+				found = true;
+			i++;
 		}
-	});
-    
-    var polyOptions = {
-	    strokeColor: '#0000ff',
-	    strokeOpacity: 1.0,
-	    strokeWeight: 3
-  	}
-    polyLine = new google.maps.Polyline(polyOptions);
-  	polyLine.setMap(map);
-  	
-  	directionsService = new google.maps.DirectionsService();
-}
-	
-function displayMarker(position) {
-	var marker;
-	marker = new google.maps.Marker({
-       	map: map, 
-       	position: position,
-    });
-        
-    google.maps.event.addListener(marker, 'click', function() {
-    	markerOnClick(this);
-  	});
-  	
-  	google.maps.event.addListener(marker, 'mouseover', function() {
-    	markerMouseOver(this);
-  	});
-  	
-  	google.maps.event.addListener(marker, 'mouseout', function() {
-    	markerMouseOut(this);
-  	});
-  	return marker;
-}
-
-function markerMouseOver(marker) {
-	marker.setIcon('images/marker_green.png');
-	$(pathByPosition[marker.position.toString()].lid).setStyle({'background': '#00ff00', 'textDecoration': 'underline'});
-}
-
-function markerMouseOut(marker) {
-	marker.setIcon('');
-	$(pathByPosition[marker.position.toString()].lid).setStyle({'background': 'transparent', 'textDecoration': 'none'});
-}
-
-function markerOnClick(marker) {
-	var path = polyLine.getPath();
-	var i = 0;
-	var found = false;
-	var inFirstPath = false;
-	
-	while (i < path.getLength() && !found) {
-		if (path.getAt(i).equals(marker.getPosition()))
-			found = true;
-		i++;
+		
+		var li = this.pathByPosition[marker.position.toString()];
+		if (li != undefined) {
+			li = $(li.lid);
+			li.toggle();
+			inFirstPath = true;
+			this.redrawPolyline(li.parentNode);
+		}
+		
+		if (!inFirstPath){
+			// Because path is an MVCArray, we can simply append a new coordinate
+	  		// and it will automatically appear
+	  		path.push(marker.getPosition());
+	  	}
 	}
 	
-	var li = pathByPosition[marker.position.toString()];
-	if (li != undefined) {
-		li = $(li.lid);
-		li.toggle();
-		inFirstPath = true;
-		redrawPolyline(li.parentNode);
-	}
+	this.generateDirections = function() {
+		var routeSheetsMap = this;
+		var path = this.polyLine.getPath();
+		var subPath = [];
 	
-	if (!inFirstPath){
-		// Because path is an MVCArray, we can simply append a new coordinate
-  		// and it will automatically appear
-  		path.push(marker.getPosition());
-  	}
-}
-
-function generateDirections() {
-	var path = polyLine.getPath();
-	var subPath = [];
-
-	path.forEach(function(position, idx) {
-		subPath.push(position);
-		//Enviamos los puntos en paquetes de a 10 para no exceder las limitaciones del geocoder
-		if (subPath.size() == 10) {
-			requestDirections(subPath);
-			subPath.clear();
+		path.forEach(function(position, idx) {
 			subPath.push(position);
+			//Enviamos los puntos en paquetes de a 10 para no exceder las limitaciones del geocoder
+			if (subPath.size() == 10) {
+				routeSheetsMap.requestDirections(subPath);
+				subPath.clear();
+				subPath.push(position);
+			}
+		});
+		
+		//Si quedó algún resto sin enviar lo enviamos ahora.
+		if (subPath.size() > 1) {
+			requestDirections(subPath);
 		}
-	});
-	
-	//Si quedó algún resto sin enviar lo enviamos ahora.
-	if (subPath.size() > 1) {
-		requestDirections(subPath);
+		
+		this.clearPolyLine();
 	}
 	
-	clearPolyLine();
-}
-
-function requestDirections(waypoints) {
-	var origin = waypoints.first();
-	var destination = waypoints.last();
+	this.requestDirections = function(waypoints) {
+		var routeSheetsMap = this;
+		var origin = waypoints.first();
+		var destination = waypoints.last();
+		
+		waypts = [];
+		var i;
+		
+		for (i = 1; i < waypoints.size() - 1; i++) {
+			waypts.push({
+				location: waypoints[i],
+				stopover: true
+			});
+		}
+		
+		var request = {
+			origin: origin, 
+			destination: destination,
+			waypoints: waypts,
+			travelMode: google.maps.DirectionsTravelMode.DRIVING
+	    };
+	    
+	    this.directionsService.route(request, function(result, status) { routeSheetsMap.displayDirections(result, status); });
+	}
 	
-	waypts = [];
-	var i;
+	this.displayDirections = function(result, status) {
+	    if (status == google.maps.DirectionsStatus.OK) {
+	    	var renderer = new google.maps.DirectionsRenderer({
+		   		suppressMarkers: true,
+		   		directions: result,
+		   		map: this.map,
+		   	});
+		   	this.directionsDisplays.push(renderer);
+	    }
+	}
 	
-	for (i = 1; i < waypoints.size() - 1; i++) {
-		waypts.push({
-			location: waypoints[i],
-			stopover: true
+	this.clearPolyLine = function() {
+		var path = this.polyLine.getPath();
+		var lenght = path.getLength();
+		var i;
+		for (i=0; i<lenght; i++) {
+			path.pop();
+		}
+	}
+	
+	this.clearAll = function() {
+		this.clearPolyLine();
+		this.clearDirections();
+	}
+	
+	this.clearDirections = function() {
+		this.directionsDisplays.each(function(renderer) {
+			renderer.setMap(null);
 		});
 	}
 	
-	var request = {
-		origin: origin, 
-		destination: destination,
-		waypoints: waypts,
-		travelMode: google.maps.DirectionsTravelMode.DRIVING
-    };
-    
-    directionsService.route(request, displayDirections);
-}
-
-function displayDirections(result, status) {
-	  	
-    if (status == google.maps.DirectionsStatus.OK) {
-    	var renderer = new google.maps.DirectionsRenderer({
-	   		suppressMarkers: true,
-	   		directions: result,
-	   		map: map,
-	   	});
-	   	directionsDisplays.push(renderer);
-    }
-}
-
-function clearPolyLine() {
-	var path = polyLine.getPath();
-	var lenght = path.getLength();
-	var i;
-	for (i=0; i<lenght; i++) {
-		path.pop();
+	this.redrawPolyline = function(list) {
+		var routeSheetsMap = this;
+		var lis = list.childElements();
+		this.clearPolyLine();
+		var path = this.polyLine.getPath();
+		
+		lis.each(function(li) {
+			if (li.visible())
+				path.push(routeSheetsMap.firstPath[li.id].position);
+		});
 	}
-}
-
-function clearAll() {
-	clearPolyLine();
-	clearDirections();
-}
-
-function clearDirections() {
-	directionsDisplays.each(function(renderer) {
-		renderer.setMap(null);
-	});
-}
-
-function redrawPolyline(list) {
-	var lis = list.childElements();
-	clearPolyLine();
-	var path = polyLine.getPath();
-	
-	lis.each(function(li) {
-		if (li.visible())
-			path.push(firstPath[li.id].position);
-	});
-}
+};
