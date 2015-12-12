ec1e8bb7681ffcae74acfab77ee579f01736f44f
dcanuhe <dcanuhe@866e3e16-41d3-4f0a-9ddd-98571eee7c8a>
2015-12-12 21:08:05 +0000

    arreglo respuesta de upload de fotos y error con primera foto de galeria

--- branches/update/WEB-INF/classes/modules/lausi/actions/LausiAddressesDoUploadPhotoAction.php
+++ branches/update/WEB-INF/classes/modules/lausi/actions/LausiAddressesDoUploadPhotoAction.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once 'LausiJsonAction.php';
-
-class LausiAddressesDoUploadPhotoAction extends LausiJsonAction {
+class LausiAddressesDoUploadPhotoAction extends BaseAction {
 
 	function execute($mapping, $form, &$request, &$response) {
 
@@ -36,5 +34,21 @@ class LausiAddressesDoUploadPhotoAction extends LausiJsonAction {
 			'Uri' => $photo->getUri()
 		];
 		echo json_encode($response);
+	}
+
+	protected function failureResponse($errorCode) {
+		switch ($errorCode) {
+			case 400:
+				header('HTTP/1.1 400 Bad Request');
+				break;
+			case 404:
+				header('HTTP/1.1 404 Not Found');
+				break;
+			case 500:
+				header('HTTP/1.1 500 Internal Server Error');
+				break;
+			default:
+				throw new Exception("unsupported error code: $errorCode");
+		}
 	}
 }
